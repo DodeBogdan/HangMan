2e3529ab8524cf33bb22daf62a9b6ed63fd762fc
Bogdan <dodebogdan@yahoo.com>
2020-03-13 11:52:31 +0200

    Modifed README

--- HangMan/HangMan.cpp
+++ HangMan/HangMan.cpp
@@ -25,99 +25,104 @@ void game(std::string str, std::string str2, bool& done)
 		done = 1;
 }
 
+void startGame()
+{
+	std::string str; // Initial string.
+	std::string str1; // Result string.
+	std::vector<std::string> vecstr; // String vector, the place where the strings are taken.
+	std::regex reg("[a-zA-Z]"); // For error checking.
+
+	std::fstream input("HangMan.txt");
+	if (!input)
+	{
+		std::cout << "There are no file!";
+		return;
+	}
+
+	if (input.peek() == std::ifstream::traits_type::eof())
+	{
+		std::cout << "The file is empty!";
+		return;
+	}
+
+	//Input from the words file.
+	while (!input.eof())
+	{
+		std::string temp;
+		input >> temp;
+		vecstr.push_back(temp);
+	}
+	input.close();
+
+	//Random word selection.
+	std::random_device dev;
+	std::mt19937 rng(dev());
+	std::uniform_int_distribution<std::mt19937::result_type> index(0, vecstr.size() - 1);
+	//srand(time(NULL));
+	//int index = rand() % (vecstr.size()-1);
+	str = vecstr[index(rng)];
+
+	//Transformation into uppercase for appearance / input.
+	std::transform(str.begin(), str.end(), str.begin(), ::toupper);
+
+	//Variables used for back exit (ENDGAME / WINGAME).
+	int mistakes = 6;
+	bool done = 0;
+
+	//Display _ _ _ depending on the length of the word.
+	std::cout << "The game has the letters: \n";
+	game(str, str1, done);
+
+	do {
+
+		std::string ch; //Used for letter input.
+		std::cout << "Input a letter: ";
+		std::getline(std::cin, ch);
+		std::transform(ch.begin(), ch.end(), ch.begin(), ::toupper); //Appearance / input transformation.
+
+		//Input verification + transformation.
+		do {
+			if (!std::regex_match(ch, reg) || ch.length() != 1) //Check if invalid characters or more than 1 character are entered.
+			{
+				std::cout << "Input invalid. Reintroduce your character: ";
+				std::getline(std::cin, ch);
+				std::transform(ch.begin(), ch.end(), ch.begin(), ::toupper);
+			}
+		} while (!std::regex_match(ch, reg) || ch.length() != 1);
+
+		if (str.find(ch) != -1) //Check if the letter entered is in the word.
+		{
+			str1.push_back(ch[0]); //It is inserted into the final string
+			game(str, str1, done);
+		}
+		else
+		{
+			mistakes--;
+			std::cout << "The letter was not found. You have " << mistakes << " mistakes available.\n\n";
+		}
+
+	} while (done != 1 && mistakes != 0);
+
+	//A message is displayed depending on the result.
+	if (mistakes == 0)
+		std::cout << "END GAME! The game was: " << str;
+	else
+		std::cout << "YOU WON!";
+}
+
 int main()
 {
-start:
-	{
-		std::string str; // Initial string.
-		std::string str1; // Result string.
-		std::vector<std::string> vecstr; // String vector, the place where the strings are taken.
-		std::regex reg("[a-zA-Z]"); // For error checking.
 
-		std::fstream input("HangMan.txt");
-		if (!input)
-		{
-			std::cout << "There are no file!";
-			return -1;
-		}
-
-		if (input.peek() == std::ifstream::traits_type::eof())
-		{
-			std::cout << "The file is empty!";
-			return -1;
-		}
-
-		//Input from the words file.
-		while (!input.eof())
-		{
-			std::string temp;
-			input >> temp;
-			vecstr.push_back(temp);
-		}
-		input.close();
-
-		//Random word selection.
-		std::random_device dev;
-		std::mt19937 rng(dev());
-		std::uniform_int_distribution<std::mt19937::result_type> index(0, vecstr.size() - 1);
-		//srand(time(NULL));
-		//int index = rand() % (vecstr.size()-1);
-		str = vecstr[index(rng)];
-
-		//Transformation into uppercase for appearance / input.
-		std::transform(str.begin(), str.end(), str.begin(), ::toupper);
-
-		//Variables used for back exit (ENDGAME / WINGAME).
-		int mistakes = 6;
-		bool done = 0;
-
-		//Display _ _ _ depending on the length of the word.
-		std::cout << "The game has the letters: \n";
-		game(str, str1, done);
-
-		do {
-
-			std::string ch; //Used for letter input.
-			std::cout << "Input a letter: ";
-			std::getline(std::cin, ch);
-			std::transform(ch.begin(), ch.end(), ch.begin(), ::toupper); //Appearance / input transformation.
-
-			//Input verification + transformation.
-			do {
-				if (!std::regex_match(ch, reg) || ch.length() != 1) //Check if invalid characters or more than 1 character are entered.
-				{
-					std::cout << "Input invalid. Reintroduce your character: ";
-					std::getline(std::cin, ch);
-					std::transform(ch.begin(), ch.end(), ch.begin(), ::toupper);
-				}
-			} while (!std::regex_match(ch, reg) || ch.length() != 1);
-
-			if (str.find(ch) != -1) //Check if the letter entered is in the word.
-			{
-				str1.push_back(ch[0]); //It is inserted into the final string
-				game(str, str1, done);
-			}
-			else
-			{
-				mistakes--;
-				std::cout << "The letter was not found. You have " << mistakes << " mistakes available.\n\n";
-			}
-
-		} while (done != 1 && mistakes != 0);
-
-		//A message is displayed depending on the result.
-		if (mistakes == 0)
-			std::cout << "END GAME! The game was: " << str;
-		else
-			std::cout << "YOU WON!";
-
-	}
+	startGame();
 	int option;
 	std::cout << "\nRetry?\n1.Yes.\n2.No.\n";
 	std::cin >> option;
 
 	if (option == 1)
-		goto start;
+	{
+		system("cls");
+		startGame();
+	}
 	else
 		std::cout << "Thank you for playing!\n";
 	return 0;
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # HangMan
+
 A project made in C++
+
+This project is made in order to learn 'regex', but also to do something fun like a game.
+
+The entire game is structured in English, and the words can be modified from the 'HangMan.txt' file.
+
+<img src = "Images/Main.png" height = 200>
+
+This is how it looks when you win.
+
+<img src = "Images/Won.png" height = 400>
+
+This is how it looks when you lose.
+
+<img src = "Images/GameOver.png" height = 400>
